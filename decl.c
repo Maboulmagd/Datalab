616ff019b60dabaf67b8f890dc14e4577d5b356e
Mohammed (Mo) Aboulmagd <Mohamedrefat007@hotmail.com>
2017-03-18 19:24:04 -0500

    datalab

---
+++ decl.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+ {"bitAnd", (funct_t) bitAnd, (funct_t) test_bitAnd, 2, "| ~", 8, 10,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"oddBits", (funct_t) oddBits, (funct_t) test_oddBits, 0,
+    "! ~ & ^ | + << >>", 8, 10,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"reverseBytes", (funct_t) reverseBytes, (funct_t) test_reverseBytes, 1,
+    "! ~ & ^ | + << >>", 25, 8,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"allEvenBits", (funct_t) allEvenBits, (funct_t) test_allEvenBits, 1,
+    "! ~ & ^ | + << >>", 12, 6,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"conditional", (funct_t) conditional, (funct_t) test_conditional, 3, "! ~ & ^ | << >>", 16, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitCount", (funct_t) bitCount, (funct_t) test_bitCount, 1, "! ~ & ^ | + << >>", 40, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"minusTwo", (funct_t) minusTwo, (funct_t) test_minusTwo, 0,
+    "! ~ & ^ | + << >>", 2, 12,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"tmax", (funct_t) tmax, (funct_t) test_tmax, 0, "! ~ & ^ | + << >>", 6, 10,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"negate", (funct_t) negate, (funct_t) test_negate, 1,
+    "! ~ & ^ | + << >>", 5, 8,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"times34", (funct_t) times34,
+     (funct_t) test_times34, 1,
+    "! ~ & ^ | + << >>", 6, 6,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isLess", (funct_t) isLess, (funct_t) test_isLess, 2,
+    "! ~ & ^ | + << >>", 24, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
